153795600026fb47f17cd579b85dc8e355e08aac
Andrew Milburn <sidbell.069@gmail.com>
2022-08-07 17:18:24 +0100

    Ch 2 [10:57 / 20:41]
    Project set up
    VSCode installed

---
+++ rust-crash-course/target/debug/deps/rust_crash_course-57c763369c9fc160.d
@@ -0,0 +1,5 @@
+c:\Users\sidbe\ActiveProjects\Vandad_Rust_Crash_Course\rust-crash-course\rust-crash-course\target\debug\deps\rust_crash_course-57c763369c9fc160.rmeta: src\main.rs
+
+c:\Users\sidbe\ActiveProjects\Vandad_Rust_Crash_Course\rust-crash-course\rust-crash-course\target\debug\deps\rust_crash_course-57c763369c9fc160.d: src\main.rs
+
+src\main.rs:
